9ea4a15419d63b6894545c0224888bb44454f492
Camila Gois de Jesus <cg160120@gmail.com>
2026-07-09 20:11:42 -0300

    chore: atualiza backend antes do rebase'

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
-from app.routers import incidents
-from app.routers import logs
-from app.routers import playbooks
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import incidents, logs, playbooks
 
 app = FastAPI(
-    title = "CyberInsight AI API",
-    version = "1.0.0"
+    title="CyberInsight AI API",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(incidents.router)
 app.include_router(logs.router)
 app.include_router(playbooks.router)
+
 
 @app.get("/")
 def root():
@@ -18,4 +32,3 @@ def root():
         "status": "online",
         "message": "CyberInsight AI API Backend funcionando!"
     }
-
